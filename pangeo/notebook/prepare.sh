302457621a294bd2f7deed6fd802cea529a93b3e
Tim Crone <tjcrone@gmail.com>
2018-07-11 11:36:01 -0400

    Add git clone

--- pangeo/notebook/prepare.sh
+++ pangeo/notebook/prepare.sh
@@ -5,6 +5,10 @@ set -x
 echo "Copy files from pre-load directory into home"
 cp --update -r -v /pre-home/. /home/jovyan
 
+# clone git repos
+git clone https://github.com/ooi-data-review/2018-data-workshops.git /home/jovyan/2018-data-workshops
+
+
 if [ -e "/opt/app/environment.yml" ]; then
     echo "environment.yml found. Installing packages"
     /opt/conda/bin/conda env update -f /opt/app/environment.yml
